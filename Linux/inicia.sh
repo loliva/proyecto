fccc4ff630d98abb8c99251f84929dc75c119de7
Luis Oliva <loliva@mackbook.local>
2016-05-09 18:32:38 -0300

    lalala

--- Linux/inicia.sh
+++ Linux/inicia.sh
@@ -2,3 +2,8 @@
 
 yum install httpd bind mysql-server squid openssl -y
 
+echo "Generando SSL para apache"
+openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout /etc/httpd/ssl/telecenter.key -out /etc/httpd/ssl/telecenter.crt
+
+echo "Copiando config httpd"
+cp -rv ./config/httpd/* /etc/httpd
